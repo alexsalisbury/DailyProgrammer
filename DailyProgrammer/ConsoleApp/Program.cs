d0d4926ec45c8145ad940a4ed00f2d71db1a648e
Alex Salisbury <alex@alexsalisbury.com>
2018-11-30 12:11:53 -0500

    Initial check-in of Console app used to run Challenge solutions.

---
+++ DailyProgrammer/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
+        }
+    }
+}
